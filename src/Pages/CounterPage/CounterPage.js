d1ef6d7117c8a368fccb1566aaab98c4f73e80b4
RomanasV <romanasvenckus@gmail.com>
2023-06-06 12:02:27 +0300

    prevState method

--- src/Pages/CounterPage/CounterPage.js
+++ src/Pages/CounterPage/CounterPage.js
@@ -6,13 +6,9 @@ const CounterPage = () => {
   const initialValue = 5;
 
   const [count, setCount] = useState(initialValue);
+  const [grades, setGrades] = useState([]);
 
-  const plus1Handler = () => setCount(count + 1);
-  const plus2Handler = () => setCount(count + 2);
-  const plus5Handler = () => setCount(count + 5);
-  const minus1Handler = () => setCount(count - 1);
-  const minus2Handler = () => setCount(count - 2);
-  const minus5Handler = () => setCount(count - 5);
+  const countButtonHandler = num => setCount(prevState => prevState + num);
   const resetHandler = () => setCount(initialValue);
 
   const inputHandler = (event) => {
@@ -25,6 +21,14 @@ const CounterPage = () => {
     } else {
       setCount(inputValue)
     }
+  }
+
+  const addGradeHandler = () => {
+    setGrades(prevState => {
+      const newState = [...prevState];
+      newState.push(count);
+      return newState;
+    });
   }
 
   const getDisplayClass = () => {
@@ -41,19 +45,34 @@ const CounterPage = () => {
     return displayClassName;
   }
 
+  const gradesList = grades && grades.length > 0 && (
+    <ul>
+      {grades.map((grade, index) => <li key={index}>{grade}</li>)}
+    </ul>
+  );
+
   return (
     <Container>
-      <h3 className={getDisplayClass()}>{count}</h3>
+      <div className="grades-form">
+        <h3 className={getDisplayClass()}>{count}</h3>
 
-      <input value={count} type="number" max="10" min="1" onChange={inputHandler} />
+        <input value={count} type="number" max="10" min="1" onChange={inputHandler} />
 
-      <button onClick={minus1Handler} disabled={count <= 1}>-1</button>
-      <button onClick={minus2Handler} disabled={count <= 2}>-2</button>
-      <button onClick={minus5Handler} disabled={count <= 5}>-5</button>
-      <button onClick={resetHandler}>Reset</button>
-      <button onClick={plus5Handler} disabled={count >= 6}>+5</button>
-      <button onClick={plus2Handler} disabled={count >= 9}>+2</button>
-      <button onClick={plus1Handler} disabled={count >= 10}>+1</button>
+        <button onClick={() => countButtonHandler(-1)} disabled={count <= 1}>-1</button>
+        <button onClick={() => countButtonHandler(-2)} disabled={count <= 2}>-2</button>
+        <button onClick={() => countButtonHandler(-5)} disabled={count <= 5}>-5</button>
+        <button onClick={resetHandler}>Reset</button>
+        <button onClick={() => countButtonHandler(5)} disabled={count >= 6}>+5</button>
+        <button onClick={() => countButtonHandler(2)} disabled={count >= 9}>+2</button>
+        <button onClick={() => countButtonHandler(1)} disabled={count >= 10}>+1</button>
+
+        <button onClick={addGradeHandler}>Add Grade</button>
+      </div>
+      
+      <div className="grades-list-wrapper">
+        <h4>{grades && grades.length > 0 ? 'Grades:' : 'No grades'}</h4>
+        {gradesList}
+      </div>
     </Container>
   )
 }
