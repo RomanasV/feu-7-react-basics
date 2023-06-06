03606eb3e876ec568c691585f66c29ab7d3c134c
RomanasV <romanasvenckus@gmail.com>
2023-06-06 10:28:04 +0300

    plus 5 and minus 5 added

--- src/Pages/CounterPage/CounterPage.js
+++ src/Pages/CounterPage/CounterPage.js
@@ -9,8 +9,10 @@ const CounterPage = () => {
 
   const plus1Handler = () => setCount(count + 1);
   const plus2Handler = () => setCount(count + 2);
+  const plus5Handler = () => setCount(count + 5);
   const minus1Handler = () => setCount(count - 1);
   const minus2Handler = () => setCount(count - 2);
+  const minus5Handler = () => setCount(count - 5);
   const resetHandler = () => setCount(initialValue);
 
   const getDisplayClass = () => {
@@ -31,9 +33,13 @@ const CounterPage = () => {
     <Container>
       <h3 className={getDisplayClass()}>{count}</h3>
 
+      <input type="number" max="10" min="1" />
+
       <button onClick={minus1Handler} disabled={count <= 1}>-1</button>
       <button onClick={minus2Handler} disabled={count <= 2}>-2</button>
+      <button onClick={minus5Handler} disabled={count <= 5}>-5</button>
       <button onClick={resetHandler}>Reset</button>
+      <button onClick={plus5Handler} disabled={count >= 6}>+5</button>
       <button onClick={plus2Handler} disabled={count >= 9}>+2</button>
       <button onClick={plus1Handler} disabled={count >= 10}>+1</button>
     </Container>
