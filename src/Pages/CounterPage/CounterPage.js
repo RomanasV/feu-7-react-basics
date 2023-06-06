c95a9825facf88f86204173ff5ee78d5a6d35b44
RomanasV <romanasvenckus@gmail.com>
2023-06-06 10:23:28 +0300

    counter task

--- src/Pages/CounterPage/CounterPage.js
+++ src/Pages/CounterPage/CounterPage.js
@@ -1,31 +1,41 @@
 import { useState } from 'react';
 import Container from '../../Components/Container/Container';
-
+import './CounterPage.css';
 
 const CounterPage = () => {
-  const [count, setCount] = useState(5);
+  const initialValue = 5;
+
+  const [count, setCount] = useState(initialValue);
 
   const plus1Handler = () => setCount(count + 1);
+  const plus2Handler = () => setCount(count + 2);
+  const minus1Handler = () => setCount(count - 1);
+  const minus2Handler = () => setCount(count - 2);
+  const resetHandler = () => setCount(initialValue);
+
+  const getDisplayClass = () => {
+    let displayClassName = ''; 
+    
+    if (count < 4) {
+      displayClassName = 'red';
+    } else if (count < 7) {
+      displayClassName = 'orange';
+    } else {
+      displayClassName = 'green';
+    }
+
+    return displayClassName;
+  }
 
   return (
     <Container>
-      <h3>{count}</h3>
+      <h3 className={getDisplayClass()}>{count}</h3>
 
-      <button>-1</button>
-      <button>-2</button>
-      <button>Reset</button>
-      <button>+2</button>
+      <button onClick={minus1Handler} disabled={count <= 1}>-1</button>
+      <button onClick={minus2Handler} disabled={count <= 2}>-2</button>
+      <button onClick={resetHandler}>Reset</button>
+      <button onClick={plus2Handler} disabled={count >= 9}>+2</button>
       <button onClick={plus1Handler} disabled={count >= 10}>+1</button>
-
-      {/* <button onClick={() => setCount(count + 1)}>+1</button> */}
-      {/* <button onClick={() => plus1Handler()}>+1</button> */}
-
-      {/* <button onClick={() => {
-        console.log(count);
-        count++;
-        console.log(count);
-      }}>+1</button> */}
-    
     </Container>
   )
 }
