a26404c2c0d3fe1d2597d402cbe419e6ea3cb712
RomanasV <romanasvenckus@gmail.com>
2023-06-13 10:44:23 +0300

    checkbox id bug fixed

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,8 +1,7 @@
 const TodoList = ({ data }) => {
-  console.log(data)
   return (
     <div className='todo-list'>
-      {data.map(todo => (
+      {data.map((todo, index) => (
           <div key={todo.id} className='todo-item'>
             <h3 className='todo-title'>{todo.title}</h3>
             <span className='todo-created-date'>Task created: {todo.date}</span>
@@ -10,8 +9,15 @@ const TodoList = ({ data }) => {
             <span className='todo-due-date'>Due date: {todo.dueDate}</span>
             
             <div className='form-control'>
-              {/* <input className='todo-done' checked={todo.done} type='checkbox' id='done' name='done' onChange={() => taskDoneHandler(todo.id)} /> */}
-              <label htmlFor='done'>Task done</label>
+              <input 
+                className='todo-done' 
+                checked={todo.done} 
+                type='checkbox' 
+                id={`done-${index}`}
+                name='done' 
+                onChange={() => console.log(todo.id)} 
+              />
+              <label htmlFor={`done-${index}`}>Task done</label>
             </div>
           </div>
         )
